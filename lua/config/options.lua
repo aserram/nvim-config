9281243194bf9c6f2a939da65c1dd839d91c84ed
Adolfo Serrano <aserranom29@gmail.com>
2026-01-19 22:33:16 -0600

    adding more custom vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,10 @@ vim.opt.shiftwidth = 4
 -- Other global options
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Search: Highlight and incremental
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Min lines above/below cursor
+vim.opt.scrolloff = 10
