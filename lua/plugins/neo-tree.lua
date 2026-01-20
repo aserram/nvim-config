5138f6fd8c7fd6d4144616b02fb289bcc4416fe1
Adolfo Serrano <aserranom29@gmail.com>
2026-01-20 14:42:38 -0600

    adding neo-tree configs

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,10 +8,16 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
-
-    config = function()
-        -- Keymap for toggling undotree
+    opts = {
+        -- Keymap for toggling neo-tree
         vim.keymap.set('n', '<leader>e', ':Neotree toggle left<CR>')
+    },
+    config = function()
+        local neo_tree = require('neo-tree')
+        neo_tree.setup({
+            -- Close Neo-tree if it is the last window left in the tab
+            close_if_last_window = true
+        })
     end,
   },
 }
