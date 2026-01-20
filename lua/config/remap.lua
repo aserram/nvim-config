7d0e83c61385812c6e51f06fbbc64b0a07045090
Adolfo Serrano <aserranom29@gmail.com>
2026-01-20 15:06:21 -0600

    adding window action remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,10 +2,19 @@
 vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
+-- Exit insert mode with jj
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
 --*** NAVIGATION ***
 -- Go back to nvim explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Exit insert mode with jj
-vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+-- Navigate between windows
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
 
+--Split windows
+vim.keymap.set('n', '<leader>sp', ':sp<CR>')
+vim.keymap.set('n', '<leader>vs', ':vs<CR>')
