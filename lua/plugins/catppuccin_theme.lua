80815a1da24cf1775ca99d7d1ac0aa011f5234de
Adolfo Serrano <aserranom29@gmail.com>
2026-01-20 00:29:25 -0600

    simplifying theme logic

--- lua/plugins/catppuccin_theme.lua
+++ lua/plugins/catppuccin_theme.lua
@@ -3,12 +3,8 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        opts = {
-            flavour = "macchiato"
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.cmd.colorscheme "catppuccin"
+        config = function()
+            vim.cmd.colorscheme "catppuccin-macchiato"
         end,
     },
 }
