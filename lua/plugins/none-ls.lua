c7b355e867be267c080038f5653e7bd518245b9a
Adolfo Serrano <aserranom29@gmail.com>
2026-01-21 15:57:03 -0600

    adding settings to python formatter & linter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,10 +10,28 @@ return {
                 -- null_ls.builtins.diagnostics.selene,
 
                 -- Python formatting and linter
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.diagnostics.pylint,
+                -- null_ls.builtins.formatting.black,
+                -- null_ls.builtins.diagnostics.pylint,
+
+                -- Python black-formatter
+                null_ls.builtins.formatting.black.with({
+                    extra_args = { "--line-length", "120" },
+                }),
+
+                -- Pythin pylint linter
+                null_ls.builtins.diagnostics.pylint.with({
+                    extra_args = {
+                        "--disable=missing-class-docstring",
+                        "--disable=missing-function-docstring",
+                        "--disable=missing-module-docstring",
+                        "--disable=invalid-name",
+                        "--extension-pkg-whitelist=pygame",
+                        "--max-line-length=120", -- match black-formatter config
+                    },
+                }),
             },
-            -- you can reuse a shared lspconfig on_attach callback here
+
+            -- Config to format on save
             on_attach = function(client, bufnr)
                 if client.supports_method("textDocument/formatting") then
                     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
