96e406f69a1af251c18ece48ce9ed6c9c747b8ea
Adolfo Serrano <aserranom29@gmail.com>
2026-01-21 13:26:37 -0600

    more remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -18,3 +18,20 @@ vim.keymap.set('n', '<C-l>', '<C-w>l')
 --Split windows
 vim.keymap.set('n', '<leader>sp', ':sp<CR>')
 vim.keymap.set('n', '<leader>vs', ':vs<CR>')
+
+-- Keep cursor centered when Page+/-
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor centered when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+-- When highlighted move up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
