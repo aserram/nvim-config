23f1f70c707114dba49515caee0258ec11b44dc2
Adolfo Serrano <aserranom29@gmail.com>
2026-01-20 18:24:03 -0600

    Virtual diagnostic
    pop up on current line
    to enable(true): virtual_lines and/or virtual_text

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -10,6 +10,7 @@ return {
             vim.api.nvim_create_autocmd('LspAttach', {
                 callback = function(args)
                     local opts = { buffer = args.buf }
+                    vim.diagnostic.config({ virtual_lines = false, virtual_text = { current_line = true } })
                     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
                     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
                 end,
