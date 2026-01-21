9e6ae36d04ba0a07324d8ee4a929002df3ba4af7
Adolfo Serrano <aserranom29@gmail.com>
2026-01-20 18:36:18 -0600

    description to lsp show diagnostic text setting

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -10,7 +10,10 @@ return {
             vim.api.nvim_create_autocmd('LspAttach', {
                 callback = function(args)
                     local opts = { buffer = args.buf }
+                    -- Add diagnostic popup -> { current_line = true } | always = true | disable = false
+                    -- virtual_lines (diagnostic below line, more details), virtual_text (diagnostic next to line, less info) 
                     vim.diagnostic.config({ virtual_lines = false, virtual_text = { current_line = true } })
+                    -- Keymaps
                     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
                     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
                 end,
