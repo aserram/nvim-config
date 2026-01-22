1703cb13eafc670f57374a9b3c0fb394d142d785
Adolfo Serrano <aserranom29@gmail.com>
2026-01-21 20:16:18 -0600

    adding config for python debug, auto venv detection

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,30 +1,44 @@
 return {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-        "mfussenegger/nvim-dap",
-        "nvim-neotest/nvim-nio",
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+        },
+
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+
+            require("dapui").setup()
+
+            -- Keymaps
+            vim.keymap.set("n", "<leader>dc", dap.continue)
+            vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+
+            -- Automatic open and close debugging window
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+        end,
     },
-
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-
-        -- Keymaps
-        vim.keymap.set("n", "<leader>dc", dap.continue)
-        vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
-
-        -- Automatic open and close debugging window
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-    end,
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        config = function()
+            require("dap-python").setup()
+        end,
+    },
 }
