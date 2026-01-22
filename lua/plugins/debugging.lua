6837be2275b4f4462c9ce15de543bc95bc19d56f
Adolfo Serrano <aserranom29@gmail.com>
2026-01-21 19:50:55 -0600

    initial commit nvim-dap for debugging

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,30 @@
+return {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+        "mfussenegger/nvim-dap",
+        "nvim-neotest/nvim-nio",
+    },
+
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+
+        -- Keymaps
+        vim.keymap.set("n", "<leader>dc", dap.continue)
+        vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+
+        -- Automatic open and close debugging window
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+    end,
+}
