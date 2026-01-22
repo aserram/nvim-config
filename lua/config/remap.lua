e4f8e24a1257e4d8607ca0b260737bf0d6000fa7
Adolfo Serrano <aserranom29@gmail.com>
2026-01-21 23:26:01 -0600

    modifying remaps to work with tmux

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -10,14 +10,14 @@ vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Navigate between windows
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
 --Split windows
-vim.keymap.set('n', '<leader>sp', ':sp<CR>')
-vim.keymap.set('n', '<leader>vs', ':vs<CR>')
+vim.keymap.set("n", "<leader>sp", ":sp<CR>")
+vim.keymap.set("n", "<leader>vs", ":vs<CR>")
 
 -- Keep cursor centered when Page+/-
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -28,10 +28,9 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Paste to system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
 
 -- When highlighted move up/down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
