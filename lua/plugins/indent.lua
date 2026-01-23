8a77f68386d1707c6471e79874d32a91853dbaa5
Adolfo Serrano <aserranom29@gmail.com>
2026-01-22 19:02:48 -0600

    changing indent config
    rainbow colores muted/thin lines

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -5,6 +5,36 @@ return {
 	---@type ibl.config
 	ops = {},
 	config = function()
-		require("ibl").setup()
+		-- local highlight = {
+		--     "CursorColumn",
+		--     "Whitespace",
+		-- }
+		local highlight = {
+			"RainbowYellow",
+			"RainbowGreen",
+			"RainbowViolet",
+			"RainbowCyan",
+			"RainbowRed",
+			"RainbowBlue",
+			"RainbowOrange",
+		}
+
+		local hooks = require("ibl.hooks")
+		-- create the highlight groups in the highlight setup hook, so they are reset
+		-- every time the colorscheme changes
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#c0b07a" })
+			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#8fb07a" })
+			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#a88ac0" })
+			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#7aa8a8" })
+			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#c06a6a" })
+			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#7aa2c0" })
+			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#c08a6a" })
+		end)
+
+		require("ibl").setup({
+			indent = { highlight = highlight, char = "▏" },
+			scope = { enabled = false },
+		})
 	end,
 }
