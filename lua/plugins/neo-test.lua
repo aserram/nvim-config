40c404c3c7c55acd9dde162e4c7a8cd9285802aa
Adolfo Serrano <aserranom29@gmail.com>
2026-01-22 22:23:43 -0600

    neo-test
    run pytest in neovim
    temp python path
    got to config so it picksup the active venv

--- lua/plugins/neo-test.lua
+++ lua/plugins/neo-test.lua
@@ -1,17 +1,29 @@
 return {
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "nvim-lua/plenary.nvim",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-neotest/neotest-python",
-    },
-    config = function()
-        require("neotest").setup({
-            adapters = {
-                require("neotest-python")({ dap = { justMyCode = false } }),
-            },
-        })
-    end,
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest-python",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-python")({
+					dap = { justMyCode = false },
+					args = { "--log-level", "DEBUG" },
+					runner = "pytest",
+					python = "venv_lnx/bin/python",
+					-- python = function()
+					-- 	local venv = os.getenv("VIRTUAL_ENV")
+					-- 	if venv and #venv > 0 then
+					-- 		return venv .. "/bin/python"
+					-- 	end
+					-- 	return "python"
+					-- end,
+				}),
+			},
+		})
+	end,
 }
