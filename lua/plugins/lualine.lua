ecf7e37ea8d6df4d006d4ed901fe81d718d45e3c
Adolfo Serrano <aserranom29@gmail.com>
2026-01-30 19:07:02 -0600

    lualine - relative filename path section c

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -55,13 +55,14 @@ return {
 				theme = my_lualine_theme,
 			},
 			sections = {
+				lualine_c = { { "filename", path = 1 } },
 				lualine_x = {
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
 						color = { fg = "#ff9e64" },
 					},
-					{ "filename", path = 2 },
+					-- { "filename", path = 2 },
 					{ "encoding" },
 					{ "fileformat", symbols = { unix = "💀" } },
 					{ "filetype" },
